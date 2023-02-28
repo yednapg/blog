fb609c6a090bab374b3699e38ed3fe7c4c01f536
Gaurav Pandey <yednapg@gmail.com>
2023-02-28 11:58:35 +0530

    fix font

--- components/header.tsx
+++ components/header.tsx
@@ -3,13 +3,13 @@ import Container from '../components/container'
 
 export default function Header() {
   return (
-    <header className="py-0">
+    <header className="py-6">
       <Container>
         <nav className="flex justify-between items-center">
-          <img src="/logo.png" alt="logo" className="img"/>
+          <Link href="/" className="nav-text">GAURAV PANDEY</Link>
           <div className="flex items-center social-icons">
-          <a href="https://github.com/yednapg"><img src="icons8-github.svg" alt="GitHub"/></a>
-          <a href="https://twitter.com/yednapg"><img src="icons8-twitter.svg" alt="Twitter"/></a>
+            <a href="https://github.com/yednapg"><img src="icons8-github.svg" alt="GitHub"/></a>
+            <a href="https://twitter.com/yednapg"><img src="icons8-twitter.svg" alt="Twitter"/></a>
           </div>
         </nav>
       </Container>
