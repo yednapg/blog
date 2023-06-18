f5772d84de23bf236f064fd4ef80d8add2bb77e9
Gaurav Pandey <hi@pandeygaurav.com>
2023-06-18 11:05:28 +0530

    add simple analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import '../public/global.css';
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Header from '../components/header'
+import type { AppProps } from "next/app";
+import type { AppProps } from "next/app";
+import React from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -20,6 +23,18 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <main className="py-7">
         <Component {...pageProps} />
       </main>
+      <React.Fragment>
+      <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js"  />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </React.Fragment>
     </>
   )
 }
