369647cfdec5c0e3f01d1cf2f656e05f48fe842b
Gaurav Pandey <yednapg@gmail.com>
2023-09-08 14:13:53 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,15 @@ function HomePage() {
             />
           </div> */}
           <p>
-            Hi 👋, I'm Gaurav. I'm a 16-year-old high schooler from New Delhi, India. I work as a community engineer at Hack Club and at CodeDay, where I work to provide welcoming and diverse opportunities for undeserved students to explore a future in tech and beyond. 
+            Hi 👋, I'm Gaurav. I'm a 16-year-old high schooler from New Delhi, India. I work as a community engineer at Hack Club. I am also a Code Evanglist at CodeDay, where I work to provide welcoming and diverse opportunities for undeserved students to explore a future in tech and beyond. 
             <br/><br/>
-            Since 2007, I've done a lot of things, from spending my 5 years of life in the snowy mountains to playing in the beautiful hills of Uttarakhand. I find joy in ambiguity and feel more engaged when working on problems that can be solved by mainly exploring the web or reading a book. 
+            Since 2007, I've done a lot of things, from spending my 5 years of life in the snowy mountains to playing in the beautiful hills of Uttarakhand. I find joy in ambiguity and feel more engaged when working on problems that can be solved by mainly exploring the web or through programming. 
             <br/><br/>
             Say Hi! at: <a href='mailto:hi@pandeygaurav.com' className='pink'>hi@pandeygaurav.com</a>
             <br/><br />
-            Note: Computers have seriously changed my life. I began coding when I was 12, and since then I've been invited to speak at international conferences in Florence, Salt Lake City, Singapore, Seattle, and London. PS: I love Vim, Music, Art and Switzerland.</p>
+            Note: Computers have seriously changed my life. I began coding when I was 12, and since then I've been invited to speak at international conferences in Florence, Salt Lake City, Singapore, Seattle, and London. 
+			<br/>< br/>
+			PS: I love Vim, Music, Art and Switzerland 💛🧡❤️💜💙🩵💚🩶.</p>
         </div>
       </Container>
     </>
