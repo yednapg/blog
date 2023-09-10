f83d1fda3223d7784104ae1945f60d94d66ea794
Gaurav Pandey <yednapg@gmail.com>
2023-09-10 07:18:23 +0530

    update text

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,13 +18,13 @@ function HomePage() {
             />
           </div> */}
           <p>
-            Hi 👋, I'm Gaurav Pandey. I'm a 16-year-old high schooler from New Delhi, India. I work as a community engineer at Hack Club. I am also a Code Evanglist at CodeDay, where I work to provide welcoming and diverse opportunities for undeserved students to explore a future in tech and beyond. 
+		  Hi 👋, I'm Gaurav. I absolutely love Generative AI and Competitive Programming. I'm a high-school student from New Delhi, India. Currently, I work as a community engineer at Hack Club, a global community for teenage hackers, and as a Code Evangelist at CodeDay where we're committed to providing inclusive and diverse opportunities for underserved students to explore a future in tech and beyond.
             <br/><br/>
-            Since 2007, I've done a lot of things, from spending my 5 years of life in the snowy mountains to playing in the beautiful hills of Uttarakhand. I find joy in ambiguity and feel more engaged when working on problems that can be solved by mainly exploring the web or through programming. 
+			Since 2007, I've embarked on various endeavors, ranging from spending five years of my life in the snowy mountains to frolicking amidst the picturesque hills of Uttarakhand. Currently I live in New Delhi. The Best Place in the World.
             <br/><br/>
             Say Hi! at: <a href='mailto:hi@pandeygaurav.com' className='pink'>hi@pandeygaurav.com</a>
             <br/><br />
-            Note: Computers have seriously changed my life. I began coding when I was 12, and since then I've been invited to speak at international conferences in Florence, Salt Lake City, Singapore, Seattle, and London. 
+			Computers have profoundly transformed my life. I started programming in Python at the age of 12, and since then, I've had the opportunity to speak at international conferences held in Florence, Salt Lake City, Singapore, Seattle, and London
 			<br/>< br/>
 			PS: I love Linux, Vim, Python, Art, and Indian Food. 💛🧡❤️💜💙🩵💚🩶</p>
         </div>
