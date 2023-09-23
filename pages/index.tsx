b8d0006dfb9a41dc0343c4a46a75671f2940c880
Gaurav Pandey <hi@pandeygaurav.com>
2023-09-23 13:09:50 +0530

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,16 +9,16 @@ function HomePage() {
           {/* <h1 className="text-2xl font-bold code-text">
            print("profile loading .. .. . 404 error")
           </h1> */}
-          {/* <div>
+          <div className="image">
             <Image
             src="/gaurav.png"
             width={1920}
             height={1080}
-            alt="Desk"
+            alt="Header Photo"
             />
-          </div> */}
+          </div>
           <p>
-		  Hi 👋, I'm Gaurav. I absolutely love Generative AI and Competitive Programming. I'm a high-school student from New Delhi, India. Currently, I work as a community engineer at Hack Club, a global community for teenage hackers, and as a Code Evangelist at CodeDay where we're committed to providing inclusive and diverse opportunities for underserved students to explore a future in tech and beyond.
+		  Hi 👋, I'm Gaurav. High-school student from New Delhi, India. Currently, I work as a community engineer at Hack Club, a global community for teenage hackers, and as a Code Evangelist at CodeDay where we're committed to providing inclusive and diverse opportunities for underserved students to explore a future in tech and beyond.
             <br/><br/>
 			Since 2007, I've embarked on various endeavors, ranging from spending five years of my life in the snowy mountains to frolicking amidst the picturesque hills of Uttarakhand. Currently I live in New Delhi. The Best Place in the World.
             <br/><br/>
@@ -26,7 +26,7 @@ function HomePage() {
             <br/><br />
 			Computers have profoundly transformed my life. I started programming in Python at the age of 12, and since then, I've had the opportunity to speak at international conferences held in Florence, Salt Lake City, Singapore, Seattle, and London
 			<br/>< br/>
-			PS: I love Linux, Vim, Python, Art, and Indian Food. 💛🧡❤️💜💙🩵💚🩶</p>
+			PS: I love Linux, Vim, Art, and Indian Food. 💛🧡❤️💜💙🩵💚🩶</p>
         </div>
       </Container>
     </>
